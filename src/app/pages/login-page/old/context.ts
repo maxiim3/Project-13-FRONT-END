fa17160ff734494e4c13c9d23fdacae6f7182150
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-02-27 16:44:19 +0100

    Removed old login page

--- src/app/pages/login-page/old/context.ts
+++
@@ -1,6 +0,0 @@
-import {createContext} from "react"
-import {formContextReducer} from "./formContextReducer"
-
-export const FormContext = createContext<ReturnType<typeof formContextReducer> | undefined>(
-	undefined as ReturnType<typeof formContextReducer> | undefined
-)
